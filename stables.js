07362f61d9c89efa662b14881eff92dc66153169
KaitlynLew <klew12@my.bcit.ca>
2024-09-19 15:47:10 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,10 @@
+console.log("horse!")
+
+let horseName = "Keeth"; // this is a comment
+horseName = "Peter";
+let horseAge = 73;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = false;
+console.log(isHorseInStable);
+
