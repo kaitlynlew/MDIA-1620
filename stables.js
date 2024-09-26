048c3f44fefea136a44c665ecb8d00fadcadb262
KaitlynLew <klew12@my.bcit.ca>
2024-09-26 14:37:24 -0700

    add operators to my stable file

--- stables.js
+++ stables.js
@@ -7,3 +7,20 @@ console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = false;
 console.log(isHorseInStable);
+
+console.log(typeof 12345);
+console.log(typeof "12345");
+console.log(typeof false);
+
+console.log(typeof horseAge);
+
+const STABLE_MONTHLY_FEE = 90;
+console.log (STABLE_MONTHLY_FEE);
+
+let horseIntroduction = " is the name if my horse";
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+let stableIntroduction = "Welcom to " + horseName + "'s stable 🦄";
+console.log(stableIntroduction);
+console.log(rentIntroduction);
+console.log(horseName + horseIntroduction);
+
